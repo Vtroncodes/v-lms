2363bd17c88f0e4028b88594ecf19601aaab67ee
punitvardaam <team@vardaam.com>
2025-01-01 19:48:56 +0530

    First

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/', function(){
+    return view('home');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
